895031ab6d9803277c8ba64ae62a6be9563db958
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-09-21 23:43:35 +0100

    simple condition for show components based on state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import UserForm from './components/UserForm';
+import ViewList from './components/ViewList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    route: 'list'
+  }
+  render() {
+    const { route } = this.state
+    return (
+      <div className="App">
+        { route === 'list' && <ViewList /> }
+        { route === 'form' && <UserForm /> }
+      </div>
+      )
+  }
 }
 
 export default App;
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class UserForm extends Component {
+    render() {
+        return(
+            'UserForm component'
+        )
+    }
+}
