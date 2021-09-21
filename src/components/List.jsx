64f9fdc73aba0c0531a4c2fa20c3f9068e2f0b14
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-09-22 00:43:15 +0100

    pass id into list with currying patter

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 export default class List extends Component {
+
+    handleClick = id => e => {
+        console.log( id );
+    }
     render() {
         const { users } = this.props
         return(
             <ul>
                 { users.map( u => 
                     <li key={u.id}>
-                    { u.name} <button>edit</button>
+                    { u.name} <button onClick={this.handleClick(u.id)}>edit</button>
                     </li>
                     ) 
                 }
